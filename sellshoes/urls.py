b0bc12ad2aae14fb824b9b0a4c578d2f423f9cd9
FRANCISMUNGANGU <francismungangu@gmail.com>
2023-03-08 15:37:25 +0300

    did more stylings

--- sellshoes/urls.py
+++ sellshoes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('show', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
-    path('delete/<int:id>', views.destory)
+    path('delete/<int:id>', views.destory),
+    path('', views.show)
 ]
